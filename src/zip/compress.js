8144e443d9751e20692647aeff9732912bbfc1ea
TarasiukDima <dimatarasiuk91@gmail.com>
2022-06-04 22:47:05 +0300

    feat: add zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,24 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import { pipeline } from 'stream';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export const compress = async () => {
-    // Write your code here 
+  const fileForZipPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const fileZipPath = path.join(__dirname, 'files', 'fileToCompress.txt.gz');
+  const errorText = 'Zip operation failed';
+  const gzip = zlib.createGzip();
+  const fileForZip = fs.createReadStream(fileForZipPath);
+  const zipFile = fs.createWriteStream(fileZipPath);
+
+  pipeline(fileForZip, gzip, zipFile, (err) => {
+    if (err) {
+      console.log(errorText);
+    }
+  });
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,24 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import { pipeline } from 'stream';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export const decompress = async () => {
-    // Write your code here 
+  const zipFilePath = path.join(__dirname, 'files', 'fileToCompress.txt.gz');
+  const unzipFilePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const errorText = 'Unzip operation failed';
+  const zipReadStream = fs.createReadStream(zipFilePath);
+  const unzipWriteStream = fs.createWriteStream(unzipFilePath);
+  const unzip = zlib.createGunzip();
+
+  pipeline(zipReadStream, unzip, unzipWriteStream, (err) => {
+    if (err) {
+      console.log(errorText);
+    }
+  });
 };
+
+decompress();
