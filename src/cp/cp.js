f3f6e5585ffe9fdb9da09ad7ef834a80b231a9de
TarasiukDima <dimatarasiuk91@gmail.com>
2022-06-05 16:09:50 +0300

    feat: add child process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,33 @@
+import cp from 'child_process';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const argsArray = process.argv.slice(2);
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const scriptPath = path.join(__dirname, 'files', 'script.js');
+  const processChild = cp.spawn(
+    'node',
+    [scriptPath, ...args],
+    { stdio: ['pipe', 'pipe', 'pipe', 'ipc'] }
+  );
+
+  process.stdin.pipe(processChild.stdin);
+
+  processChild.stdout.pipe(process.stdout);
+
+  processChild.stdout.on('data', (data) => {
+    process.stdout.write(`Data from child process: ${data.toString()}\n`);
+  });
+
+  processChild.stderr.on('data', () => {
+    console.error('Failed stderr in child process.')
+  })
+
+  processChild.on('error', (_) => {
+    process.stderr.write('Failed to start child process.');
+  });
 };
+
+spawnChildProcess(argsArray);
